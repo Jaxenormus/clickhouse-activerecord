14d20b55177bd3274fb8a6150179c001fe88557c
Caleb <me@calebdelbridge.com>
2026-08-29 16:51:31 -0400

    Revert "fix: preserve ClickHouse default expressions"
    This reverts commit 75ed41d4209daa57669c1e319deaa0a516923f18.

--- spec/single/schema_statements_spec.rb
+++ spec/single/schema_statements_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe 'ActiveRecord::ConnectionAdapters::Clickhouse::SchemaStatements' 
 
   describe '#projections' do
     before do
-      connection.execute('CREATE TABLE projection_test (id UInt64, value Float64 DEFAULT 0, started_on Date, happened_on Date32, happened_at DateTime64(6, \'UTC\'), boundary MultiPolygon, PROJECTION by_id (SELECT * ORDER BY id)) ENGINE = MergeTree ORDER BY id')
+      connection.execute('CREATE TABLE projection_test (id UInt64, value Float64, started_on Date, happened_on Date32, happened_at DateTime64(6, \'UTC\'), boundary MultiPolygon, PROJECTION by_id (SELECT * ORDER BY id)) ENGINE = MergeTree ORDER BY id')
     end
 
     after { connection.execute('DROP TABLE IF EXISTS projection_test') }
@@ -28,7 +28,7 @@ RSpec.describe 'ActiveRecord::ConnectionAdapters::Clickhouse::SchemaStatements' 
       schema = StringIO.new
       ClickhouseActiverecord::SchemaDumper.dump(connection, schema)
 
-      expect(schema.string).to include('t.column "value", "Float64", default: -> { "0" }')
+      expect(schema.string).to include('t.column "value", "Float64"')
       expect(schema.string).to include('t.column "started_on", "Date"')
       expect(schema.string).to include('t.column "happened_on", "Date32"')
       expect(schema.string).to include('t.column "happened_at", "DateTime64(6, \'UTC\')"')
